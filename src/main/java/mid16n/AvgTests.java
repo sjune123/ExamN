21c90173af051bc64510f39479d6888e20b44aa6
Administrator <Administrator@305-19>
2016-04-18 11:49:08 +0900

    1292080 천성준
    중간고사 테스트

---
+++ src/main/java/mid16n/AvgTests.java
@@ -0,0 +1,15 @@
+package mid16n;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AvgTests {
+
+	@Test
+	public void test() {
+
+	@assertEquals(80,getResult());
+	}
+
+}
